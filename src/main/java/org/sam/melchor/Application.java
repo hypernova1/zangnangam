f20bd0558099c5d1d5ae573562e5fc5a84bf755f
sam <chtlstjd01@gmail.com>
2020-01-09 22:41:23 +0900

    프로젝트 생성

---
+++ src/main/java/org/sam/melchor/Application.java
@@ -0,0 +1,13 @@
+package org.sam.melchor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelchorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelchorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sam/melchor/MelchorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sam.melchor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelchorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
