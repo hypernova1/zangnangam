28ff32cb3915210c5be23f609506f6c2a3ccbed1
sam <chtlstjd01@gmail.com>
2020-02-04 22:33:04 +0900

    xss 테스트 삭제

--- src/main/java/org/sam/melchor/config/HtmlCharacterEscapes.java
+++ src/main/java/org/sam/melchor/config/HtmlCharacterEscapes.java
@@ -4,15 +4,10 @@ import com.fasterxml.jackson.core.SerializableString;
 import com.fasterxml.jackson.core.io.CharacterEscapes;
 import com.fasterxml.jackson.core.io.SerializedString;
 import org.apache.commons.text.StringEscapeUtils;
-import org.apache.commons.text.translate.AggregateTranslator;
-import org.apache.commons.text.translate.CharSequenceTranslator;
-import org.apache.commons.text.translate.EntityArrays;
-import org.apache.commons.text.translate.LookupTranslator;
 
 public class HtmlCharacterEscapes extends CharacterEscapes {
 
     private final int[] asciiEscapes;
-    private CharSequenceTranslator translator;
 
     public HtmlCharacterEscapes() {
         // 1. XSS 방지 처리할 특수 문자 지정
